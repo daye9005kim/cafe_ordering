9a1fc6042eee2c54e609dd12825adc2d3722e9a8
sjr4746 <sjr4746@jasonmg.com>
2021-02-15 15:56:05 +0900

    스타벅스 음료 fetch

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -5,6 +5,9 @@ class Welcome extends MY_Controller
 {
     public function index()
     {
+
+
+        var_dump($this->Starbucks_model->fetch());
         return $this->load->view('view', array('status' => 200, 'data' => array()));
     }
 }
