01878831a67fd79e5d9da8464f05be06bf53a6fd
sjr4746 <sjr4746@jasonmg.com>
2021-02-16 17:18:11 +0900

    Member 컨트롤러

---
+++ application/controllers/Member.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Member extends MY_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+
+    }
+
+    public function login()
+    {
+
+    }
+
+    public function login_ok()
+    {
+
+    }
+
+    public function logout()
+    {
+
+    }
+}
