e0224063fad03832b37b714274c7394d68ea4062
KimDaYe <daye.kim@jasongroup.co.kr>
2021-03-15 16:04:12 +0900

    스타일변경

--- application/views/member/login.php
+++ application/views/member/login.php
@@ -73,9 +73,9 @@ include_once APPPATH . 'views/_common/header.php';
 		})
 	</script>
 <body>
-	<div class="form-inline" style="width: 90%; text-align: center; margin:auto; margin-top: 20px;">
+	<div class="form-inline" style="width: 90%; text-align: left; margin:auto; margin-top: 20px;">
 		<p class="label label-info" style="font-size: medium;">해당하는 주문을 선택하세요.</p>
-		<div style="margin: 10px">
+		<div style="margin: 10px;">
 			<ul class="list-unstyled">
 				<?php
 				foreach ($data['order_list'] as $item) {
