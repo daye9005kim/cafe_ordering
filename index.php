8d00e01674a6d649e8af4fdfd235fab3112274f2
KimDaYe <daye.kim@jasongroup.co.kr>
2021-12-15 18:31:50 +0900

    Revert "CI_ENV 설정"
    This reverts commit b5b63162

--- index.php
+++ index.php
@@ -53,7 +53,6 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	(include_once '.CI_ENV.php') || die('not found .CI_ENV.php');
 	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
 
 /*
